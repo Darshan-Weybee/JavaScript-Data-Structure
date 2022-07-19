1bf36cb79c2bc46773c0f78bfb87b2a363abda00
Darshan Kagathara <darshan@webtheuniverse.com>
2022-07-19 11:03:12 +0530

    Completed

--- Extra_Assignment_script.js
+++ Extra_Assignment_script.js
@@ -13,300 +13,298 @@
 // .toLowerCase,   .toUpperCase,
 // .length, indexOf(),   lastindexOf()
 
-stringMethod = {
-    str: "darshan",
-    str1 : "kagathara",
-    sliceMethod: function(first, second = "a"){
-        let len = this.str.length;
-        if(second == "a"){
-            let st = "";
-            if(0 <= first && first < len){
-                for(let i=first; i<len; i++){
-                    st = st + this.str[i];
-                }
-                return st;
+String.prototype.sliceMethod = function(first, second = "a"){
+    let len = this.length;
+    if(second == "a"){
+        let st = "";
+        if(0 <= first && first < len){
+            for(let i=first; i<len; i++){
+                st = st + this[i];
             }
-            else{
-                for(let i=len-Math.abs(first); i<len; i++){
-                    st = st + this.str[i];
-                }
-                return st;
-            }
+            return st;
         }
         else{
-            let st = "";
-            if(0 <=first && first <len && 0 <=second && second < len){
-                for(let i=first; i<second; i++){
-                    st = st + this.str[i];
-                }
-                return st;
+            for(let i=len-Math.abs(first); i<len; i++){
+                st = st + this[i];
             }
-            else if(first<0 && second<0){
-                for(let i=len-Math.abs(first); i<len-Math.abs(second); i++){
-                    st = st + this.str[i];
-                }
-                return st;
-            }
-            else{
-                for(let i=first; i<len-Math.abs(second); i++){
-                    st = st + this.str[i];
-                }
-                return st;
-            }
+            return st;
         }
-    },
-    SubstringMethod: function(first, second = "a"){
-        let len = this.str.length;
-        if(second == "a"){
-            let st = "";
-            if(0 <= first && first < len){
-                for(let i=first; i<len; i++){
-                    st = st + this.str[i];
-                }
-                console.log(st);
+    }
+    else{
+        let st = "";
+        if(0 <=first && first <len && 0 <=second && second < len){
+            for(let i=first; i<second; i++){
+                st = st + this[i];
             }
-            else{
-                for(let i=0; i<len; i++){
-                    st = st + this.str[i];
-                }
-                console.log(st);
+            return st;
+        }
+        else if(first<0 && second<0){
+            for(let i=len-Math.abs(first); i<len-Math.abs(second); i++){
+                st = st + this[i];
             }
+            return st;
         }
         else{
-            let st = "";
-            if(0 <=first && first <len && 0 <=second && second < len){
-                for(let i=first; i<second; i++){
-                    st = st + this.str[i];
-                }
-                console.log(st);
+            for(let i=first; i<len-Math.abs(second); i++){
+                st = st + this[i];
             }
-            else if(first>0 && second<0){
-                for(let i=0; i<first; i++){
-                    st = st + this.str[i];
-                }
-                console.log(st);
-            }
+            return st;
         }
-    },
-    substrMethod: function(first, second = "a"){
-        let len = this.str.length;
-        if(second == "a"){
-            let st = "";
-            if(0 <= first && first < len){
-                for(let i=first; i<len; i++){
-                    st = st + this.str[i];
-                }
-                console.log(st);
+    }
+}
+String.prototype.substringMethod = function(first, second = "a"){
+    let len = this.length;
+    if(second == "a"){
+        let st = "";
+        if(0 <= first && first < len){
+            for(let i=first; i<len; i++){
+                st = st + this[i];
             }
-            else{
-                for(let i=len-Math.abs(first); i<len; i++){
-                    st = st + this.str[i];
-                }
-                console.log(st);
-            }
+            console.log(st);
         }
         else{
-            let st = "";
-            if(0 <= first && first < len){
-                for(let i=first; i<first+second; i++){
-                    st = st + this.str[i];
-                }
-                console.log(st);
+            for(let i=0; i<len; i++){
+                st = st + this[i];
             }
-            else{
-                for(let i=len-Math.abs(first); i<len-Math.abs(first)+second; i++){
-                    st = st + this.str[i];
-                }
-                console.log(st);
-            }
+            console.log(st);
         }
-    },
-    replaceMethod: function(word, newWord){
-        let len = this.str.length;
-        let wordlength = word.length;
-        let firstIndex;
-        let lastIndex;
-        for(let i=0; i<len; i++){
-            if(word[0] == this.str[i]){
-                let check = "";
-                for(let j=i; j<i+wordlength; j++){
-                    check = check + this.str[j];
-                }
-                if(check == word){
-                    firstIndex = i;
-                    break;
-                }
-            }
-        }
-        lastIndex = firstIndex + wordlength;
-        let pre = ""; 
-        let last = "";
-        for(let i=0; i<firstIndex; i++){
-            pre = pre + this.str[i];
-        }
-        for(let i=lastIndex; i<len; i++){
-            last = last + this.str[i];
-        }
-        console.log(pre + newWord + last);
-
-    },
-    concatMethod: function(word){
-        console.log(this.str + word);
-    },
-    trimMethod: function(){
-        let len  = this.str.length;
+    }
+    else{
         let st = "";
-        console.log(this.str);
-        for(let i=0; i<len; i++){
-            if(this.str[i] != " "){
-                st = st + this.str[i];
+        if(0 <=first && first <len && 0 <=second && second < len){
+            for(let i=first; i<second; i++){
+                st = st + this[i];
             }
+            console.log(st);
         }
-        console.log(st);
-    },
-    padStartMethod: function(second, first){
-        let len = this.str.length;
-        let netlength = second - len;
-        let st = "";
-        for(let i=0; i<netlength; i++){
-            st = st + first;
-        }
-        console.log(st + this.str);
-    },
-    padEndMethod: function(second, first){
-        let len = this.str.length;
-        let netlength = second - len;
-        let st = "";
-        for(let i=0; i<netlength; i++){
-            st = st + first;
-        }
-        console.log(this.str + st);
-    },
-    repeateMethod: function(num){
-        for(let i=0; i<num; i++){
-            console.log(this.str);
-        }
-    },
-    splitMethod: function(first){
-        let len = this.str.length;
-        let arr = [];
-        let j = 0;
-        let index = 0;
-        for(let i=0; i<len; i++){
-            if(this.str[i] == first){
-                let st = "";
-                for(let k=index; k<i; k++){
-                    st = st + this.str[k];
-                }
-                arr[j] = st;
-                j++;
-                index = i+1;
+        else if(first>0 && second<0){
+            for(let i=0; i<first; i++){
+                st = st + this[i];
             }
+            console.log(st);
         }
-        for(let i=len-1; i>=0; i--){
-            if(this.str[i]==first){
-                let st = ""
-                for(let k=len-1; k>i; k--){
-                    st = this.str[k] + st;
-                }
-                arr[j] = st;
+    }
+}
+String.prototype.substrMethod = function(first, second = "a"){
+    let len = this.length;
+    if(second == "a"){
+        let st = "";
+        if(0 <= first && first < len){
+            for(let i=first; i<len; i++){
+                st = st + this[i];
+            }
+            console.log(st);
+        }
+        else{
+            for(let i=len-Math.abs(first); i<len; i++){
+                st = st + this[i];
+            }
+            console.log(st);
+        }
+    }
+    else{
+        let st = "";
+        if(0 <= first && first < len){
+            for(let i=first; i<first+second; i++){
+                st = st + this[i];
+            }
+            console.log(st);
+        }
+        else{
+            for(let i=len-Math.abs(first); i<len-Math.abs(first)+second; i++){
+                st = st + this[i];
+            }
+            console.log(st);
+        }
+    }
+}
+String.prototype.replaceMethod = function(word, newWord){
+    let len = this.length;
+    let wordlength = word.length;
+    let firstIndex;
+    let lastIndex;
+    for(let i=0; i<len; i++){
+        if(word[0] == this[i]){
+            let check = "";
+            for(let j=i; j<i+wordlength; j++){
+                check = check + this[j];
+            }
+            if(check == word){
+                firstIndex = i;
                 break;
             }
         }
-        console.log(arr);
-    },
-    includesMethod: function(word){
-        let len = this.str.length;
-        let wordlength = word.length;
-        for(let i=0; i<len; i++){
-            if(word[0] == this.str[i]){
-                let check = "";
-                for(let j=i; j<i+wordlength; j++){
-                    check = check + this.str[j];
-                }
-                if(check == word){
-                    console.log(true);
-                    break;
-                }
-            }
-            if(i == len-1)
-                console.log(false);
+    }
+    lastIndex = firstIndex + wordlength;
+    let pre = ""; 
+    let last = "";
+    for(let i=0; i<firstIndex; i++){
+        pre = pre + this[i];
+    }
+    for(let i=lastIndex; i<len; i++){
+        last = last + this[i];
+    }
+    console.log(pre + newWord + last);
+
+}
+String.prototype.concatMethod = function(word){
+    console.log(this + word);
+}
+String.prototype.trimMethod = function(){
+    let len  = this.length;
+    let st = "";
+    console.log(this);
+    for(let i=0; i<len; i++){
+        if(this[i] != " "){
+            st = st + this[i];
         }
-    },
-    startswithMethod:function(word){
-        let len = this.str.length;
-        let wordlength = word.length;
-            if(word[0] == this.str[0]){
-                let check = "";
-                for(let j=0; j<wordlength; j++){
-                    check = check + this.str[j];
-                }
-                if(check == word){
-                    console.log(true);
-                }
-                else
-                    console.log(false);
-            }
-            else
-                console.log(false);
-    },
-    endswithMethod:function(word){
-        let len = this.str.length;
-        let wordlength = word.length;
-            if(word[wordlength-1] == this.str[len-1]){
-                let check = "";
-                for(let j=len-1; j>=len-wordlength; j--){
-                    check =  this.str[j] + check;
-                }
-                if(check == word){
-                    console.log(true);
-                }
-                else
-                    console.log(false);
-            }
-            else
-                console.log(false);
-    },
-    lengthMethod: function(){
-        let count = 0;
-        while(this.str[count] != undefined){
-            count++;
-        }
-        console.log(count);
-    },
-    indexofMethod: function(word){
-        let len = this.str.length;
-        let wordlength = word.length;
-        for(let i=0; i<len; i++){
-            if(word[0] == this.str[i]){
-                let check = "";
-                for(let j=i; j<i+wordlength; j++){
-                    check = check + this.str[j];
-                }
-                if(check == word){
-                    console.log(i);
-                    break;
-                }
-            }
-            else if(i == len-1)
-                console.log(-1);
-        }
-    },
-    lastindexofMethod: function(word){
-        let len = this.str.length;
-        let wordlength = word.length;
-        let index = -1;
-        for(let i=0; i<len; i++){
-            if(word[0] == this.str[i]){
-                let check = "";
-                for(let j=i; j<i+wordlength; j++){
-                    check = check + this.str[j];
-                }
-                if(check == word){
-                    index = i;
-                }
-            }
-        }
-        console.log(index);
+    }
+    console.log(st);
+}
+String.prototype.padStartMethod = function(second, first){
+    let len = this.length;
+    let netlength = second - len;
+    let st = "";
+    for(let i=0; i<netlength; i++){
+        st = st + first;
+    }
+    console.log(st + this);
+}
+String.prototype.padEndMethod = function(second, first){
+    let len = this.length;
+    let netlength = second - len;
+    let st = "";
+    for(let i=0; i<netlength; i++){
+        st = st + first;
+    }
+    console.log(this + st);
+}
+String.prototype.repeateMethod = function(num){
+    for(let i=0; i<num; i++){
+        console.log(String(this));
     }
 }
+String.prototype.splitMethod = function(first){
+    let len = this.length;
+    let arr = [];
+    let j = 0;
+    let index = 0;
+    for(let i=0; i<len; i++){
+        if(this[i] == first){
+            let st = "";
+            for(let k=index; k<i; k++){
+                st = st + this[k];
+            }
+            arr[j] = st;
+            j++;
+            index = i+1;
+        }
+    }
+    for(let i=len-1; i>=0; i--){
+        if(this[i]==first){
+            let st = ""
+            for(let k=len-1; k>i; k--){
+                st = this[k] + st;
+            }
+            arr[j] = st;
+            break;
+        }
+    }
+    console.log(arr);
+}
+String.prototype.includesMethod = function(word){
+    let len = this.length;
+    let wordlength = word.length;
+    for(let i=0; i<len; i++){
+        if(word[0] == this[i]){
+            let check = "";
+            for(let j=i; j<i+wordlength; j++){
+                check = check + this[j];
+            }
+            if(check == word){
+                console.log(true);
+                break;
+            }
+        }
+        if(i == len-1)
+            console.log(false);
+    }
+}
+String.prototype.startswithMethod = function(word){
+    let len = this.length;
+    let wordlength = word.length;
+        if(word[0] == this[0]){
+            let check = "";
+            for(let j=0; j<wordlength; j++){
+                check = check + this[j];
+            }
+            if(check == word){
+                console.log(true);
+            }
+            else
+                console.log(false);
+        }
+        else
+            console.log(false);
+}
+String.prototype.endswithMethod = function(word){
+    let len = this.length;
+    let wordlength = word.length;
+        if(word[wordlength-1] == this[len-1]){
+            let check = "";
+            for(let j=len-1; j>=len-wordlength; j--){
+                check =  this[j] + check;
+            }
+            if(check == word){
+                console.log(true);
+            }
+            else
+                console.log(false);
+        }
+        else
+            console.log(false);
+}
+String.prototype.lengthMethod = function(){
+    let count = 0;
+    while(this[count] != undefined){
+        count++;
+    }
+    console.log(count);
+}
+String.prototype.indexofMethod = function(word){
+    let len = this.length;
+    let wordlength = word.length;
+    for(let i=0; i<len; i++){
+        if(word[0] == this[i]){
+            let check = "";
+            for(let j=i; j<i+wordlength; j++){
+                check = check + this[j];
+            }
+            if(check == word){
+                console.log(i);
+                break;
+            }
+        }
+        else if(i == len-1)
+            console.log(-1);
+    }
+}
+String.prototype.lastindexofMethod = function(word){
+    let len = this.length;
+    let wordlength = word.length;
+    let index = -1;
+    for(let i=0; i<len; i++){
+        if(word[0] == this[i]){
+            let check = "";
+            for(let j=i; j<i+wordlength; j++){
+                check = check + this[j];
+            }
+            if(check == word){
+                index = i;
+            }
+        }
+    }
+    console.log(index);
+}
+
+
